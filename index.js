1546eb70f053b7529079a1b859cc86e235449ac2
sam ditson <samditson2@hotmail.co.uk>
2018-07-30 18:44:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,4 @@ var height = "74"
 module.exports = { name, height
 }
 if (name === "Susan") {
-    console.log("The name is correct")
-} else { console.log("Expected the name to be Susan")}
+    console.log(" name is
