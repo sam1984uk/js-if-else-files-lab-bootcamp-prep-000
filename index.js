28f92396e6031af36a84b02423c4b33e0794577a
sam ditson <samditson2@hotmail.co.uk>
2018-07-30 19:03:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ var height = "70"
 // Don't worry about this
 module.exports = { name, height
 }
+if (name === "Susan"){console.log("The name is correct")}
+if (height === 70){console.log("The height is correct")}
